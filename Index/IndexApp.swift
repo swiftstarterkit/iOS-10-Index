d6f9eec83716606693eb4427db958dee8715589f
Pao Yu <paoyu@Paos-MacBook-Pro.local>
2020-07-31 11:09:08 -0400

    first commit

---
+++ Index/IndexApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndexApp.swift
+//  Index
+//
+//  Created by Pao Yu on 2020-07-31.
+//
+
+import SwiftUI
+
+@main
+struct IndexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
